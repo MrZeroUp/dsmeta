690ccea914a9c0d01e35ff187df0d364f4d952f5
Alisson Matheus <umnordestinoyt@gmail.com>
2022-11-18 00:18:09 -0300

    Project created

---
+++ backend/src/test/java/com/devmr/dsmeta/DsmetaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devmr.dsmeta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsmetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
